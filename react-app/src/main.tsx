95823790f26a29fcd296c4849ccf0ba8cb9c6098
taka-0305 <81013183+taka-0305@users.noreply.github.com>
2025-02-15 12:22:29 +0900

    feat:cssをレスポンシブ対応、styles/_media.scssに@mixinを記述し、includeすることで対応

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/destyle.css'
-import './index.css'
+import './index.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
